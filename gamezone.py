a08b244c734696c54f30a0a678d6d3c7820f9423
Julio Pochet Edmead <jpe3841@gmail.com>
2020-02-11 15:13:18 -0500

    add gamezone.py file, and edited the license file

---
+++ gamezone.py
@@ -0,0 +1,12 @@
+import sys
+import time
+
+def loading_game():
+    print("LOADING GAME...")
+    time.sleep(2)
+    print("THE GAME WILL START IN 3 SECONDS...")
+    time.sleep(3)
+    print("Opps Nevermind! I haven't finish coding the game yet lol")
+
+
+loading_game()
